fdf47c28e4a10c7d2767234a0c9fb3d0d2f31151
somarawat <somarawat2812@gmail.com>
2025-08-27 16:43:44 +0530

    exp1

---
+++ exp1.2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("gg1 sapling estate\ncivil hospital road\nlandour\nmussoorie\n ");
+    return 0;
+}
---
+++ exp1.3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() 
+{
+ int age;
+printf("enter your age:");
+scanf("%d",&age);
+printf("your age is %d",age);
+char name[50];
+printf("\n enter your name:");
+scanf("%s", name);
+printf("your name is %s",name);
+return 0;
+}
